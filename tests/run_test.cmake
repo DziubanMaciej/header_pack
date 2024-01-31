64200e20e9de97f1850b1781e871a4c5870976a4
MaciejDziuban <dziuban.maciej@gmail.com>
2024-01-31 18:18:25 +0100

    Try compiling generated header during testing

--- tests/run_test.cmake
+++ tests/run_test.cmake
@@ -3,8 +3,8 @@ set(TEST_SOURCE_DIR "${CMAKE_CURRENT_LIST_DIR}/${TEST_NAME}")
 file(READ "${TEST_SOURCE_DIR}/arguments" ARGS)
 separate_arguments(ARGS UNIX_COMMAND "${ARGS}")
 
-set(EXPECTED_OUTPUT_PATH "${TEST_SOURCE_DIR}/expected_output")
-set(ACTUAL_OUTPUT_PATH "${WORK_DIR}/output")
+set(EXPECTED_OUTPUT_PATH "${TEST_SOURCE_DIR}/expected_output.h")
+set(ACTUAL_OUTPUT_PATH "${WORK_DIR}/output.h")
 
 # Prepare output directory
 file(REMOVE_RECURSE "${WORK_DIR}")
@@ -22,6 +22,16 @@ execute_process(
 )
 if (NOT ${HEADER_PACK_RESULT} STREQUAL 0)
     message(FATAL_ERROR "header_pack failed with a result value of ${HEADER_PACK_RESULT}")
+endif()
+
+# Try to compile
+file(WRITE "${WORK_DIR}/main.cpp" "#include \"output.h\"\n")
+execute_process(
+    COMMAND ${COMPILER_PATH} ${COMPILER_ARGS} main.cpp
+    WORKING_DIRECTORY "${WORK_DIR}"
+    RESULT_VARIABLE COMPILATION_RESULT)
+if (NOT ${COMPILATION_RESULT} STREQUAL 0)
+    message(FATAL_ERROR "compilation of generated header file failed with a result value of ${COMPILATION_RESULT}")
 endif()
 
 # Verify output file
